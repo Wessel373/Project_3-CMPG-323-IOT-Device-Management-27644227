494f7a18a2ccc495123846e29b4a2e81622850cb
Wessel373 <wespieters37@gmail.com>
2022-09-26 21:02:20 +0200

    Added Zones and Device interfaces

--- WebApp_CMPG323_27644227/DeviceManagement_WebApp/Repositories/ZoneRepository.cs
+++ WebApp_CMPG323_27644227/DeviceManagement_WebApp/Repositories/ZoneRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace DeviceManagement_WebApp.Repositories
+﻿using DeviceManagement_WebApp.Data;
+using DeviceManagement_WebApp.Models;
+using System.Linq;
+
+namespace DeviceManagement_WebApp.Repositories
 {
-    public class ZoneRepository
+    public class ZoneRepository : GennericRepository<Zone>, IZoneRepository
     {
+        public ZoneRepository(WebApp_CMPG323_27644227_Context _context) : base(_context)
+        {
+        }
+        public Zone GetMostRecentZone()
+        {
+            return _context.Zone.OrderByDescending(zone => zone.DateCreated).FirstOrDefault();
+        }
     }
 }
